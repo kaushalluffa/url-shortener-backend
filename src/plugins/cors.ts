fdad79f17b29b3778e25bce3559a3f6cd872daca
kaushalluffa <kaushalluffa91@gmail.com>
2025-03-07 19:53:51 -0500

    fix: update cors plugin

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,12 +1,17 @@
-import cors, { FastifyCorsOptions } from '@fastify/cors'
-
-export const autoConfig: FastifyCorsOptions = {
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
+import cors, { FastifyCorsOptions } from "@fastify/cors";
+import fp from "fastify-plugin";
 
 /**
  * This plugins enables the use of CORS.
  *
  * @see {@link https://github.com/fastify/fastify-cors}
  */
-export default cors
+export default fp(async (fastify) => {
+  const autoConfig: FastifyCorsOptions = {
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: fastify.config.CORS_ORIGIN_URL,
+    credentials: true,
+  };
+  fastify.log.info(autoConfig);
+  await fastify.register(cors, autoConfig);
+});
