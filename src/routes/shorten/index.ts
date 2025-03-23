9f03143e84bff882bbf52543c0a533f4e8ae4efb
kaushalluffa <kaushalluffa91@gmail.com>
2025-03-22 20:23:53 -0400

    fix: update index file for shorten routes

--- src/routes/shorten/index.ts
+++ src/routes/shorten/index.ts
@@ -3,8 +3,30 @@ import shorten from "./shorten.js";
 import redirect from "./redirect.js";
 import { shortenSchema } from "../../schemas/shorten/shorten.js";
 import { redirectSchema } from "../../schemas/shorten/redirect.js";
+import { listUrls } from "./list.js";
 
 export default async function shortenRoutes(fastify: FastifyInstance) {
-  fastify.post("/shorten", { schema: shortenSchema }, shorten);
-  fastify.get("/:short_url", { schema: redirectSchema }, redirect);
+  fastify.post(
+    "/create-short-url",
+    { onRequest: [fastify.authenticate], schema: shortenSchema },
+    shorten
+  );
+  fastify.get(
+    "/list-urls",
+    {
+      onRequest: [fastify.authenticate],
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            page: { type: "string" },
+            perPage: { type: "string" },
+            search: { type: "string" }
+          },
+        },
+      },
+    },
+    listUrls
+  );
+  fastify.get("/redirect/:short_url", { schema: redirectSchema }, redirect);
 }
