7497f75d6f8b594449c49f7de3a051a33fb3299e
kaushalluffa <kaushalluffa91@gmail.com>
2025-03-22 20:20:28 -0400

    fix: fix userId key name

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ declare module "fastify" {
 declare module "@fastify/jwt" {
   interface FastifyJWT {
     user: {
-      user_id: string;
+      userId: string;
       email: string;
       name: string;
     };
